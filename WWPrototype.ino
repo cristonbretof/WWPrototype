1ce7081d6797e7d3d747b9b846d7075a4646dda4
Mathias <mathias.st-laurent.1@ulaval.ca>
2021-03-14 15:19:18 -0400

    Modification de last_output

--- WWPrototype.ino
+++ WWPrototype.ino
@@ -32,7 +32,6 @@
 #define OUTPUT_MAX   5        // Value max of variable output in apply_PID(float Vin)
 #define OUTPUT_MIN   0        // Value max of variable output in apply_PID(float Vin)
 
-#define Last_output  0        //Dernière valeur de output
 
 //This is the I2C Address of the MCP4725, by default (A0 pulled to GND).
 //Please note that this breakout is for the MCP4725A0. 
@@ -41,6 +40,8 @@
 
 static float int_err = 0;
 static float prev_err = 0;
+
+float last_output = 0; //Dernière valeur de output
 
 int lookup = 0;// Pour test CAD varaible for navigating through the tables
 float sintab2[100] = 
@@ -219,7 +220,7 @@ void loop() {
   {
     Vin = buffer.pop();
     output = apply_PID(Vin);
-	Last_output = output;
+	last_output = output;
     lcd.setCursor(0,0);
     lcd.print("PWM = "); lcd.print(output,4);
     lcd.setCursor(0,1);
