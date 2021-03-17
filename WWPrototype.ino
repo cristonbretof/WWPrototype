a96d34d53e574bfe9de8f5dcab4283e7547af7e2
SimonLepage13 <simon.lepage.3@ulaval.ca>
2021-03-17 18:13:08 -0400

    PID presque final

--- WWPrototype.ino
+++ WWPrototype.ino
@@ -5,83 +5,92 @@
 
 #include <Wire.h> //Library for DAC
 
-#define LED_PIN       13       // GPIO pin (on-board LED)
+#define LED_PIN 13 // GPIO pin (on-board LED)
 
-#define V0            2.5      // Voltage refering to home position of platform
-#define Vlim_Up       2        // Maximum allowed voltage to provide to amplifier
+#define V0 2.5    // Voltage refering to home position of platform
+#define Vlim_Up 2 // Maximum allowed voltage to provide to amplifier
 
-#define pinADC        A15      // Analog pin to read position input
-#define ADC_RES       1023     // Maximum resolution of analog to digital converter
-#define VREF          5        // ADC reference voltage
+#define pinADC A15   // Analog pin to read position input
+#define ADC_RES 1023 // Maximum resolution of analog to digital converter
+#define VREF 5       // ADC reference voltage
 
-#define pinCURRENT    A14      // 
-#define PWM_PIN       12       // PWM pin to control amplifier (basic PWM)
+#define pinCURRENT A14 //
+#define PWM_PIN 12     // PWM pin to control amplifier (basic PWM)
 
-#define PRESCALER     256      // Prescaler digital value
-#define BOARD_Freq    16000000 // Arduino Mega 2560 board frequency in Hz
-#define MAX_FREQ      129      // Absolute maximum frequency with minimal LCD use
-#define FREQUENCY     10       // Currently used frequency
+#define PRESCALER 256       // Prescaler digital value
+#define BOARD_Freq 16000000 // Arduino Mega 2560 board frequency in Hz
 
-#define ENABLE_TIMER  TIMSK1 | (1 << OCIE1A)
+#define MAX_FREQ 145  // Absolute maximum frequency with minimal LCD use
+#define FREQUENCY 145 // Currently used frequency
+
+#define ENABLE_TIMER TIMSK1 | (1 << OCIE1A)
 #define DISABLE_TIMER TIMSK1 | (0 << OCIE1A)
 
-#define MASS_ERROR    0.3
-#define GRAMS_TO_OZ   0.035274
+#define MASS_ERROR 0.3
+#define GRAMS_TO_OZ 0.035274
 
-#define Kp            0.01     // Proportional gain
-#define Ki            0.2      // Integral gain
-#define Kd            0.001    // Differential gain
+/*#define Ku            0.008     // Ultimate gain
+#define tu            0.136     //Periode oscillations
+#define Ki            Kp/(tu/2)        // Integral gain
+#define Kd            Kp*(tu/3)         // Differential gain
+#define Kp            Ku/3 */
+// Proportional gain
 
-#define pinRS         24      
-#define pinE          26
-#define pinD4         28
-#define pinD5         30
-#define pinD6         32
-#define pinD7         34
+#define Kp 0.008
+#define Ki 0.06
+#define Kd 0.00003
 
-#define NUM_TYPES     7
-#define NUM_UNITS     2
+#define pinRS 24
+#define pinE 26
+#define pinD4 28
+#define pinD5 30
+#define pinD6 32
+#define pinD7 34
 
-#define DAC_RES       4096     // Maximum resolution of digital to analog converter
-#define DAC_VREF      5        // DAC reference voltage
-#define OUTPUT_MAX    2        // Offre une protection sur la valeur appliquer au DAC.
-#define OUTPUT_MIN    0        // Offre une protection sur la valeur appliquer au DAC.
+#define NUM_TYPES 7
+#define NUM_UNITS 2
+
+#define DAC_RES 4096 // Maximum resolution of digital to analog converter
+#define DAC_VREF 5   // DAC reference voltage
+#define OUTPUT_MAX 2 // Offre une protection sur la valeur appliquer au DAC.
+#define OUTPUT_MIN 0 // Offre une protection sur la valeur appliquer au DAC.
 
 //This is the I2C Address of the MCP4725, by default (A0 pulled to GND).
-//Please note that this breakout is for the MCP4725A0. 
-#define MCP4725_ADDR  0x60   
+//Please note that this breakout is for the MCP4725A0.
+#define MCP4725_ADDR 0x60
 //For devices with A0 pulled HIGH, use 0x61
 
+#define NUM_BUTTONS 4
 
-#define NUM_BUTTONS     4
+#define pinBUTTON_A 18
+#define pinBUTTON_B 2
+#define pinBUTTON_MENU 19
+#define pinBUTTON_TARE 3
 
-#define pinBUTTON_A     18  
-#define pinBUTTON_B     2
-#define pinBUTTON_MENU  19
-#define pinBUTTON_TARE  3
-
-#define BUF_LEN         20
-#define NUM_ETALONS     11
-#define AVG_SAMPLES     3
+#define BUF_LEN 20
+#define NUM_ETALONS 11
+#define AVG_SAMPLES 3
 
 #define INTERCEPT_CALCULATION_INDEX 5
 
-typedef enum {
+typedef enum
+{
   CONFIG_STATE = 0,
   SCALE_STATE = 1,
   BENCHMARK_STATE = 2,
   PROCESS_STATE = 3
 } state_t;
 
-typedef enum {
+typedef enum
+{
   MODE_NORMAL = 0,
   MODE_MOYENNAGE = 1,
   MODE_ETALONNAGE = 2
 } mode_t;
 
 /* Symbol arrays */
-String typeArray[NUM_TYPES] = {"1¢","5¢","10¢","25¢","1$","2$","Ø"};
-String unitArray[NUM_UNITS] = {"oz","g"};
+String typeArray[NUM_TYPES] = {"1c", "5c", "10c", "25c", "1$", "2$", "0"};
+String unitArray[NUM_UNITS] = {"oz", "g"};
 
 float massTypeGram[NUM_TYPES] = {2.35, 3.95, 1.75, 4.4, 6.9, 6.27, 6.92};
 float massTypeOunce[NUM_TYPES] = {0.07054792, 0.1058219, 0.03527396, 0.141096, 0.211644, 0.2116438, 0.2440958};
@@ -115,7 +124,7 @@ static state_t currentState = CONFIG_STATE;
 static mode_t selectedMode = MODE_NORMAL;
 
 /* Array of all button pins */
-const uint8_t inputPins[NUM_BUTTONS] = {pinBUTTON_MENU,pinBUTTON_B,pinBUTTON_A,pinBUTTON_TARE};
+const uint8_t inputPins[NUM_BUTTONS] = {pinBUTTON_MENU, pinBUTTON_B, pinBUTTON_A, pinBUTTON_TARE};
 
 /* Error values declarations */
 static float int_err = 0;
@@ -133,46 +142,41 @@ void setupTimer();
 
 /* Up and down LCD character definitions */
 byte up[8] = {
-  B00000,
-  B00100,
-  B01110,
-  B11111,
-  B01110,
-  B01110,
-  B00000,
-  B00000
-};
+    B00000,
+    B00100,
+    B01110,
+    B11111,
+    B01110,
+    B01110,
+    B00000,
+    B00000};
 byte down[8] = {
-  B00000,
-  B00000,
-  B01110,
-  B01110,
-  B11111,
-  B01110,
-  B00100,
-  B00000
-};
+    B00000,
+    B00000,
+    B01110,
+    B01110,
+    B11111,
+    B01110,
+    B00100,
+    B00000};
 byte full[8] = {
-  B11111,
-  B11111,
-  B11111,
-  B11111,
-  B11111,
-  B11111,
-  B11111,
-  B11111
-};
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111};
 byte stable[8] = {
-  B00000,
-  B00000,
-  B01010,
-  B01010,
-  B00000,
-  B10001,
-  B01110,
-  B00000
-};
-
+    B00000,
+    B00000,
+    B01010,
+    B01010,
+    B00000,
+    B10001,
+    B01110,
+    B00000};
 
 CircularBuffer<uint16_t, AVG_SAMPLES> avgBuffer;
 CircularBuffer<float, BUF_LEN> scaleBuffer;
@@ -185,7 +189,8 @@ LiquidCrystal lcd = LiquidCrystal(pinRS, pinE, pinD4, pinD5, pinD6, pinD7);
 // ARDUINO FONCTIONS //
 ///////////////////////
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   /* Create both up and down arrows */
@@ -207,10 +212,8 @@ void setup() {
   currentState = CONFIG_STATE;
   statePtr = configState;
 
-  setupTimer();
-
   /* Start CAD */
-  Wire.begin() ;
+  Wire.begin();
 }
 
 void loop()
@@ -218,22 +221,21 @@ void loop()
   statePtr();
 }
 
-
 ///////////////////////
 //  PRINT FONCTIONS  //
 ///////////////////////
 void printScaleFirstLine(void)
 {
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("MASSE: ");
-  lcd.print(currMass,2);
+  lcd.print(currMass, 2);
   lcd.print(" ");
   lcd.print(currentUnit);
 }
 
 void printScaleSecondLine(void)
 {
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print(currNumCoins);
   lcd.print(" x ");
   lcd.print(currentType);
@@ -241,97 +243,97 @@ void printScaleSecondLine(void)
 
 void printMenuConfig(void)
 {
-  switch(selectedMode)
+  switch (selectedMode)
   {
-    case MODE_NORMAL:
-      lcd.clear();
-      
-      eraseArrowUp();
-      printArrowDown();
-      
-      lcd.setCursor(0,0);
-      lcd.print("MODE NORMAL");
-      break;
-    case MODE_MOYENNAGE:
-      lcd.clear();
-      
-      printArrowUp();
-      printArrowDown();
-      
-      lcd.setCursor(0,0);
-      lcd.print("MODE MOYENNAGE");
-      break;
-    case MODE_ETALONNAGE:
-      lcd.clear();
-      
-      printArrowUp();
-      eraseArrowDown();
-      
-      lcd.setCursor(0,0);
-      lcd.print("ETALONNAGE");
-      break;
+  case MODE_NORMAL:
+    lcd.clear();
+
+    eraseArrowUp();
+    printArrowDown();
+
+    lcd.setCursor(0, 0);
+    lcd.print("MODE NORMAL");
+    break;
+  case MODE_MOYENNAGE:
+    lcd.clear();
+
+    printArrowUp();
+    printArrowDown();
+
+    lcd.setCursor(0, 0);
+    lcd.print("MODE MOYENNAGE");
+    break;
+  case MODE_ETALONNAGE:
+    lcd.clear();
+
+    printArrowUp();
+    eraseArrowDown();
+
+    lcd.setCursor(0, 0);
+    lcd.print("ETALONNAGE");
+    break;
   }
 }
 
 void printStabilitySymbol(void)
 {
-  lcd.setCursor(15,1);
+  lcd.setCursor(15, 1);
   lcd.write(byte(3));
 }
 
 void eraseStabilitySymbol(void)
 {
-  lcd.setCursor(15,1);
+  lcd.setCursor(15, 1);
   lcd.write(" ");
 }
 
 void printBenchmarkSteps(void)
 {
-    lcd.clear();   
-    lcd.setCursor(0,0);
-    if (currentStep > 0 && !benchmarkBuffer.isFull())
-    {
-      lcd.print("Posez une masse");
-      lcd.setCursor(0,1);
-      lcd.print("de : ");
-      lcd.print(currentStep);
-      lcd.print("g");
-    }
-    else if (currentStep == 0)
-    {
-      lcd.print("Retirez le poids");
-      lcd.setCursor(0,1);
-      lcd.print("de la balance");
-    }
-    else
-    {
-      lcd.print("Retirez le poids");
-      lcd.setCursor(0,1);
-      lcd.print("de la balance");
-    }
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  if (currentStep > 0 && !benchmarkBuffer.isFull())
+  {
+    lcd.print("Posez une masse");
+    lcd.setCursor(0, 1);
+    lcd.print("de : ");
+    lcd.print(currentStep);
+    lcd.print("g");
+  }
+  else if (currentStep == 0)
+  {
+    lcd.print("Retirez le poids");
+    lcd.setCursor(0, 1);
+    lcd.print("de la balance");
+  }
+  else
+  {
+    lcd.print("Retirez le poids");
+    lcd.setCursor(0, 1);
+    lcd.print("de la balance");
+  }
 }
 
 void printArrowUp()
 {
-  lcd.setCursor(15,0);
+  lcd.setCursor(15, 0);
   lcd.write(byte(0));
 }
 
 void printArrowDown()
 {
-  lcd.setCursor(15,1);
+  lcd.setCursor(15, 1);
   lcd.write(byte(1));
 }
 
 void eraseArrowUp()
 {
-  lcd.setCursor(15,0);
+  lcd.setCursor(15, 0);
   lcd.write(" ");
 }
 
 void eraseArrowDown()
 {
-  lcd.setCursor(15,1);
+  lcd.setCursor(15, 1);
   lcd.write(" ");
 }
 
@@ -346,11 +348,14 @@ void eraseArrowDown()
             capteur de position. Elle prend aussi comme argument la tension
             de référence qui décrit la position x0 de la lame.
 */
-static float apply_PID(float Vin){
+static float apply_PID(float Vin)
+{
+  delay(10);
   float output;
 
   // Calculate positionning error
   float err = V0 - Vin;
+  Serial.println(err);
 
   // Record previous integral error to allow reset in case of windup
   float temp_int = int_err;
@@ -370,24 +375,33 @@ static float apply_PID(float Vin){
   output = proportional_part + integral_part + differential_part;
   if (output >= Vlim_Up || output <= -Vlim_Up)
   {
-  	Serial.println("Windup");
-  	int_err = temp_int; //Remet l'ancienne int_erre
+    Serial.println("Windup");
+    int_err = temp_int; //Remet l'ancienne int_erre
     // Set value to absolute max
     integral_part = (Ki * int_err * (float)(1 / (float)(FREQUENCY)));
     output = proportional_part + integral_part + differential_part;
   }
- 
+
   output = last_output - output; //Corrige la dernière tension appliquée.
-  
-  //Le if, else if qui suit offre une protection pour de pas avoir des valeurs trop haute ou trop base. Important de la laisser car il arrive que la première erreur donne une erreur complètement erroné et que le PID diverge.  
+
+  if (abs(output) < (5 / ADC_RES))
+  {
+    Serial.println("haha");
+    return 0.0;
+  }
+  //Le if, else if qui suit offre une protection pour de pas avoir des valeurs trop haute ou trop base. Important de la laisser car il arrive que la première erreur donne une erreur complètement erroné et que le PID diverge.
   if (output < OUTPUT_MIN) // Vérifie que output respecte ça valeur min
   {
-	  output = OUTPUT_MIN;
+    Serial.println("Output min");
+
+    output = OUTPUT_MIN;
   }
   else if (output > OUTPUT_MAX) // Vérifie que output respecte ça valeur max
   {
-	  output = OUTPUT_MAX;
-  }  
+    Serial.println("Output max");
+
+    output = OUTPUT_MAX;
+  }
   sendToDAC(output);
   return output;
 }
@@ -402,7 +416,7 @@ void setupTimer()
   TCNT1 = 0;
 
   /* Set compare register for desired frequency */
-  OCR1A = (int)(BOARD_Freq/PRESCALER/FREQUENCY-1);
+  OCR1A = (int)(BOARD_Freq / PRESCALER / FREQUENCY - 1);
 
   TCCR1B |= (1 << WGM12);  // Turn on CTC mode
   TCCR1B |= (1 << CS12);   // Set prescaler to 8
@@ -415,7 +429,7 @@ void setupTimer()
 void incrementBenchmark(uint16_t val)
 {
   if (!benchmarkBuffer.isFull())
-  {    
+  {
     currentStep = tabEtalons[step_index];
     benchmarkBuffer.push(val);
     step_index++;
@@ -429,8 +443,8 @@ void calculateAvgMass(void)
   {
     sum += avgBuffer.pop();
   }
-  
-  currMass = (float)(penteMasseCourant*(sum/AVG_SAMPLES) + ordMasseCourant);
+
+  currMass = (float)(penteMasseCourant * (sum / AVG_SAMPLES) + ordMasseCourant);
   if (currentUnit == "oz")
   {
     currMass *= GRAMS_TO_OZ;
@@ -439,21 +453,22 @@ void calculateAvgMass(void)
 
 void calculateNumCoins(void)
 {
-  currNumCoins = floor(currMass/massTypeGram[type_index]);
+  currNumCoins = floor(currMass / massTypeGram[type_index]);
 }
 
-void sendToDAC(float outputDAC){
-	/* Fonction qui permet de prendre un float et de communiquer avec le DAC with I2C */
-	
-	last_output = outputDAC;
-	
-	int outputDACint = round(outputDAC * (DAC_RES/DAC_VREF)); // Converti l'argument transmit en int en fonction de la résolution et de la valeur de référence du DAC
-	
-	Wire.beginTransmission(MCP4725_ADDR);
-	Wire.write(64);                     // cmd to update the DAC
-	Wire.write(outputDACint >> 4);        // the 8 most significant bits...
-	Wire.write((outputDACint & 15) << 4); // the 4 least significant bits...
-	Wire.endTransmission();
+void sendToDAC(float outputDAC)
+{
+  /* Fonction qui permet de prendre un float et de communiquer avec le DAC with I2C */
+
+  last_output = outputDAC;
+
+  int outputDACint = round(outputDAC * (DAC_RES / DAC_VREF)); // Converti l'argument transmit en int en fonction de la résolution et de la valeur de référence du DAC
+
+  Wire.beginTransmission(MCP4725_ADDR);
+  Wire.write(64);                       // cmd to update the DAC
+  Wire.write(outputDACint >> 4);        // the 8 most significant bits...
+  Wire.write((outputDACint & 15) << 4); // the 4 least significant bits...
+  Wire.endTransmission();
 }
 
 /////////////////////
@@ -475,29 +490,29 @@ static void processState(void)
 
   /* Remise zéro de l'indice d'étape */
   step_index = 0;
-  
+
   lcd.clear();
-  
+
   /* Calcul de la pente pour le calcul de la masse */
-  for(int i = 0; i < NUM_ETALONS; i++)                         // Moyenne des coefficients
+  for (int i = 0; i < NUM_ETALONS; i++) // Moyenne des coefficients
   {
     if (i == INTERCEPT_CALCULATION_INDEX - 1)
     {
-      xCurrentForIntercept = (benchmarkBuffer.pop()*VREF)/ADC_RES;
-      sommePentes += tabEtalons[i]/xCurrentForIntercept;
+      xCurrentForIntercept = (benchmarkBuffer.pop() * VREF) / ADC_RES;
+      sommePentes += tabEtalons[i] / xCurrentForIntercept;
     }
     else
     {
-      sommePentes += tabEtalons[i]/((benchmarkBuffer.pop()*VREF)/ADC_RES);
+      sommePentes += tabEtalons[i] / ((benchmarkBuffer.pop() * VREF) / ADC_RES);
     }
-    
+
     /* Petite animation durant l'état de processus */
-    lcd.setCursor(i,i%2);
+    lcd.setCursor(i, i % 2);
     lcd.print(byte(2));
     delay(500);
   }
-  penteMasseCourant = sommePentes/NUM_ETALONS;
-  ordMasseCourant = tabEtalons[5] - penteMasseCourant*xCurrentForIntercept;
+  penteMasseCourant = sommePentes / NUM_ETALONS;
+  ordMasseCourant = tabEtalons[5] - penteMasseCourant * xCurrentForIntercept;
 
   /* Réétablir la tare */
   massTare = currMass;
@@ -517,8 +532,8 @@ static void benchmarkState(void)
   if (!scaleBuffer.isEmpty())
   {
     /* Debug de la fréquence en se référant à la taille du buffer (nb d'élément dans le buffer à cet instant) */
-    Serial.println(scaleBuffer.size()); // On veut la valeur de 1, pas de 20, sur le moniteur
-    
+    //Serial.println(scaleBuffer.size()); // On veut la valeur de 1, pas de 20, sur le moniteur
+
     Vin = scaleBuffer.pop();
     apply_PID(Vin); //Vin, converti en volt
   }
@@ -527,12 +542,12 @@ static void benchmarkState(void)
 static void scaleState(void)
 {
   float Vin = 0;
-  
+
   if (!scaleBuffer.isEmpty())
   {
     /* Debug de la fréquence en se référant à la taille du buffer (nb d'élément dans le buffer à cet instant) */
-    Serial.println(scaleBuffer.size()); // On veut la valeur de 1, pas de 20, sur le moniteur
-    
+    // Serial.println(scaleBuffer.size()); // On veut la valeur de 1, pas de 20, sur le moniteur
+
     Vin = scaleBuffer.pop();
     apply_PID(Vin); //Vin, converti en volt
     if (selectedMode == MODE_MOYENNAGE)
@@ -543,7 +558,7 @@ static void scaleState(void)
       }
       else
       {
-        avgBuffer.push(analogRead(pinCURRENT)); 
+        avgBuffer.push(analogRead(pinCURRENT));
       }
     }
   }
@@ -575,31 +590,32 @@ ISR(TIMER1_COMPA_vect)
   uint16_t val = 0;
 
   val = analogRead(pinADC);
-  scaleBuffer.push((float)((val)*VREF/ADC_RES));
+  scaleBuffer.push((float)((val)*VREF / ADC_RES));
 }
 
 void ISR_menuSelect(void) // Ou select
 {
-  if(currentState == SCALE_STATE)
+  if (currentState == SCALE_STATE)
   {
     currentState = CONFIG_STATE;
     statePtr = configState;
   }
-  else if(currentState == CONFIG_STATE)
+  else if (currentState == CONFIG_STATE)
   {
-    if(selectedMode == MODE_ETALONNAGE)
+    setupTimer();
+    if (selectedMode == MODE_ETALONNAGE)
     {
       currentState = BENCHMARK_STATE;
       statePtr = benchmarkState;
     }
     else
     {
-      
+
       currentState = SCALE_STATE;
       statePtr = scaleState;
     }
   }
-  else if(currentState == BENCHMARK_STATE)
+  else if (currentState == BENCHMARK_STATE)
   {
     currentState = CONFIG_STATE;
     statePtr = configState;
@@ -608,16 +624,16 @@ void ISR_menuSelect(void) // Ou select
 
 void ISR_buttonA(void) // Ou up
 {
-  if(currentState == SCALE_STATE)
+  if (currentState == SCALE_STATE)
   {
     unit_index++;
     currentUnit = unitArray[unit_index];
-    if (unit_index == NUM_UNITS-1)
+    if (unit_index == NUM_UNITS - 1)
     {
       unit_index = 0;
     }
   }
-  else if(currentState == CONFIG_STATE)
+  else if (currentState == CONFIG_STATE)
   {
     if (selectedMode == MODE_MOYENNAGE)
     {
@@ -632,18 +648,18 @@ void ISR_buttonA(void) // Ou up
 
 void ISR_buttonB(void) // Ou down
 {
-  if(currentState == SCALE_STATE)
+  if (currentState == SCALE_STATE)
   {
     type_index++;
     currentType = typeArray[type_index];
-    if (type_index == NUM_TYPES-1)
+    if (type_index == NUM_TYPES - 1)
     {
       type_index = 0;
     }
   }
-  else if(currentState == CONFIG_STATE)
+  else if (currentState == CONFIG_STATE)
   {
-    if(selectedMode == MODE_NORMAL)
+    if (selectedMode == MODE_NORMAL)
     {
       selectedMode = MODE_MOYENNAGE;
     }
@@ -656,7 +672,7 @@ void ISR_buttonB(void) // Ou down
 
 void ISR_tare(void)
 {
-  if(currentState == SCALE_STATE)
+  if (currentState == SCALE_STATE)
   {
     massTare = currMass;
   }
@@ -670,7 +686,7 @@ void ISR_tare(void)
     else
     {
       uint16_t val = analogRead(pinCURRENT);
-      incrementBenchmark(val); 
+      incrementBenchmark(val);
     }
   }
 }
